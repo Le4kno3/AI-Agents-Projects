cf655f3577a5a5b8285719c2a4daa9b9997a91a6
le4kno3 <le4kno3@gmail.com>
2026-08-03 05:24:41 +0530

    updates

--- Restuarant-Chatbot-AI-Agent/main.py
+++ Restuarant-Chatbot-AI-Agent/main.py
@@ -1,9 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-
-## Fetch the CSV file, and store each line in an array
-with open("realistic_restaurant_reviews.csv", "r") as file:
-    reviews = file.readlines()
+from vector import retriever
 
 model = OllamaLLM(model="llama3.1:8b")
 
@@ -18,18 +15,27 @@ Here is the question to answer: {question}
 
 prompt = ChatPromptTemplate.from_template(queryTemplate)
 
+# chain the prompt and model
 chain = prompt | model
 
 while True:
-    print("\n\n--------------------------------------------------------")
+    print("\n\n--------------------------------------------------------\n")
     user_input = input("Enter your question (or type 'exit' to quit): ")
-    print("\n\n")
+    print("\n--------------------------------------------------------\n")
     # while loop break condition
     if user_input.lower() in ['exit', 'quit', 'q']:
         break
-    # setting the inputs to query
+
+    # Lookup for relevant documents (reviews) based on the user input
+    # How many reviews to retrieve, will depend on the "search_kwargs".
+    # retriever will automatically embed the user input.
+    reviews = retriever.invoke(user_input)
+
+    # run the model, and pass only relevant reviews, based on the user.
     result = chain.invoke({
-        "reviews": reviews,     # without using the power of vector database / vector search, hence speed is slow
+        "reviews": reviews,
         "question": user_input
     })
+
+    # print the results
     print(result)
